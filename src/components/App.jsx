3282d2099d2d690c4cd7e1781387962ed7aec5b4
BarbaraBravoRedondo <barbarabravoredondo@gmail.com>
2023-10-16 11:15:25 +0200

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,81 +1,195 @@
 //imports depencencies,images,styles*//
 
-// import { useState } from 'react'
-// import  from '../images/react.svg'
-// import  from 
-import '../styles/App.scss'
+import { useEffect, useState } from 'react';
+
+import '../styles/App.scss';
 
 function App() {
-
   //funcions,vars,handles
+  const [countryList, setCountryList] = useState([]);
+  const[country,setCountry] = useState("");
+  const[continent,setContinent] = useState("");
+  const[newCountry,setNewCountry] = useState([{
+    "name": {
+    "common": "",
+    "official": "",
+    "nativeName": {
+    "fra": {
+    "official": "",
+    "common": ""
+    }
+    }
+    },
+    "capital": [
+    ""
+    ],
+    "flag": "",
+    "continents": [
+    ""
+    ]
+    }]);
+    const [error,setError] = useState('')
 
+  useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all?fields=name,capital,flag,continents')
+      .then((response) => response.json())
+      .then((data) => setCountryList(data));
+  }, []);
+
+  const handleClick=() => {
+    ev.preventDefault();
+    if(newCountry.name==='' || newCountry.capital===''|| newCountry.flag===''||newCountry.continents[0])
+    { setError('Rellena todos los campos')}
+  
+  else { 
+  setCountryList([...countryList, newCountry]);
+  setNewCountry([{
+    "name": {
+    "common": "",
+    "official": "",
+    "nativeName": {
+    "fra": {
+    "official": "",
+    "common": ""
+    }
+    }
+    },
+    "capital": [
+    ""
+    ],
+    "flag": "",
+    "continents": [
+    ""
+    ]
+    }])}}
+
+const handleNewCountry=(ev)=>{
+   const cloneNewCountry={...newCountry}
+   cloneNewCountry[ev.target.id]=ev.target.value;
+   setNewCountry(cloneNewCountry)
+}
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  const filterCountry = (ev) => {
+    setCountry(ev.target.value);
+  };
+  
+ const filterContinent = (ev) => {
+    setContinent(ev.target.value);
+  };
+
+
+  const renderList = () => {
+    return countryList
+
+
+    .filter((eachCountry) =>
+    eachCountry.name.official.toLowerCase().includes(country.toLowerCase()))
+
+     .filter ((eachCountry)=> ( eachCountry.continents[0].includes(continent))
+   )
+
+
+    .map((eachCountry, index) => (
+    <ul key={index}>
+    <li>
+      <span>{eachCountry.flag}</span>
+      <p>{eachCountry.name.official}</p>
+      <span>{eachCountry.capital}</span>
+      <span>{eachCountry.continents}</span>
+    </li>
+    </ul>
+    ))
+  };
 
   return (
-  
-  <>
-   <div className="page">
+    <>
       <header>
-        <h1 className="header__title">Juego del ahorcado</h1>
+        <h1>Country Info App</h1>
+        <p>
+          Explore informaction about countries, capitals and flags.Add new
+          countries and filter through the list!{' '}
+        </p>
       </header>
-      <main className="main">
+      <main>
         <section>
-          <div className="solution">
-            <h2 className="title">Solución:</h2>
-            <ul className="letters">
-              <li className="letter">k</li>
-              <li className="letter">a</li>
-              <li className="letter"></li>
-              <li className="letter">a</li>
-              <li className="letter">k</li>
-              <li className="letter">r</li>
-              <li className="letter"></li>
-              <li className="letter">k</li>
-              <li className="letter">e</li>
-              <li className="letter">r</li>
-            </ul>
-          </div>
-          <div className="error">
-            <h2 className="title">Letras falladas:</h2>
-            <ul className="letters">
-              <li className="letter">f</li>
-              <li className="letter">q</li>
-              <li className="letter">h</li>
-              <li className="letter">p</li>
-              <li className="letter">x</li>
-            </ul>
-          </div>
-          <form className="form">
-            <label className="title" for="last-letter">Escribe una letra:</label>
-            <input
-              autocomplete="off"
-              className="form__input"
-              maxlength="1"
-              type="text"
-              name="last-letter"
-              id="last-letter"
-            />
+          <form onSubmit={handleSubmit}>
+            <h2>Filters</h2>
+            <label htmlFor="country">
+              By Country
+              <input
+                type="text"
+                name="country"
+                placeholder="Spain.."
+                onChange={filterCountry}
+                value={country.name} />
+            
+            </label>
+            <label htmlFor="continent">
+              By Continent:
+              <select
+                name="continent"
+                id="continent"
+                onChange={filterContinent}
+                value={continent.continents}
+              >
+                <option value="">All</option>
+                <option value="Africa">Africa</option>
+                <option value="North America">North America</option>
+                <option value="South America">South America</option>
+                <option value="Europe">Europe</option>
+                <option value="Asia">Asia</option>
+                <option value="Oceania">Oceania</option>
+              </select>
+            </label>
           </form>
+          <div>{renderList()}</div>
         </section>
-        <section className="dummy error-5">
-          <span className="error-13 eye"></span>
-          <span className="error-12 eye"></span>
-          <span className="error-11 line"></span>
-          <span className="error-10 line"></span>
-          <span className="error-9 line"></span>
-          <span className="error-8 line"></span>
-          <span className="error-7 line"></span>
-          <span className="error-6 head"></span>
-          <span className="error-5 line"></span>
-          <span className="error-4 line"></span>
-          <span className="error-3 line"></span>
-          <span className="error-2 line"></span>
-          <span className="error-1 line"></span>
+        <section>
+<form action="" onSubmit={handleSubmit}>
+  <h2>Add Country</h2>
+  <input 
+  type="text"
+  placeholder="Country Name"
+  name="name"
+  value={newCountry.name}
+  id={newCountry.flag}
+  onChange={handleNewCountry}
+   />
+  <input 
+  type="text" 
+  name="capital"
+  placeholder="Capital"
+  value={newCountry.capital}
+  id={newCountry.flag}
+  onChange={handleNewCountry}/>
+  <input 
+  type="text" 
+  placeholder="Flag Icon"
+  name="flag"
+  id={newCountry.flag}
+  value={newCountry.flag}
+  onChange={handleNewCountry}
+  />
+  <input 
+  type="text"
+  placeholder="Continent"
+  name="continent"
+  id={newCountry.flag}
+  value={newCountry.continents}
+  onChange={handleNewCountry}/>
+<button onClick={handleClick}>Add Country</button>
+
+
+
+</form>
+
         </section>
       </main>
-    </div>
-     
-   </>
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
